58d53b9fc7b8546ce59e55fc76421e602170a896
Maria Paz Riquelme <isabella.rimed@gmail.com>
2025-05-15 14:02:30 +0200

    Primer commit - ejercicios FullstackOpen Parte 1

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client'
+/* import App from './unicafe/App' */
+import App from './anecdotes/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
